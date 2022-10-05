477d11cb1d733fd42c1ae3cbd0ec646cbfa7073a
amldimsed <ads,dark@gmail.com>
2022-10-05 00:22:49 -0400

    instrucción goto

---
+++ pregunta1/InstruccionGoTo/Form1.Designer.vb
@@ -0,0 +1,147 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(244, 43)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(43, 27)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(54, 43)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(130, 52)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "menor a mayor"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(244, 87)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(43, 27)
+        Me.TextBox2.TabIndex = 2
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(244, 133)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(43, 27)
+        Me.TextBox3.TabIndex = 3
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(322, 43)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(43, 27)
+        Me.TextBox4.TabIndex = 4
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(322, 87)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(43, 27)
+        Me.TextBox5.TabIndex = 5
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(322, 133)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(43, 27)
+        Me.TextBox6.TabIndex = 6
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(54, 120)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(130, 52)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "mayor a menor"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(244, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(60, 20)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Entrada"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(322, 13)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(50, 20)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Salida"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(426, 229)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ pregunta1/InstruccionGoTo/Form1.vb
@@ -0,0 +1,77 @@
+﻿Public Class Form1
+    Dim n(2) As Integer
+    Dim t As Integer
+
+
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim cont As Integer
+
+        n(0) = Convert.ToInt32(TextBox1.Text)
+        n(1) = Convert.ToInt32(TextBox2.Text)
+        n(2) = Convert.ToInt32(TextBox3.Text)
+        cont = 1
+suma:
+
+        If cont <= 3 Then
+            If n(0) > n(1) Then
+                t = n(0)
+                n(0) = n(1)
+                n(1) = t
+            End If
+
+            If n(1) > n(2) Then
+                t = n(1)
+                n(1) = n(2)
+                n(2) = t
+            End If
+            cont = cont + 1
+            GoTo suma
+        End If
+
+        TextBox4.Text = n(0)
+        TextBox5.Text = n(1)
+        TextBox6.Text = n(2)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim cont As Integer
+
+        n(0) = Convert.ToInt32(TextBox1.Text)
+        n(1) = Convert.ToInt32(TextBox2.Text)
+        n(2) = Convert.ToInt32(TextBox3.Text)
+        cont = 1
+suma:
+
+        If cont <= 3 Then
+            If n(0) < n(1) Then
+                t = n(0)
+                n(0) = n(1)
+                n(1) = t
+            End If
+
+            If n(1) < n(2) Then
+                t = n(1)
+                n(1) = n(2)
+                n(2) = t
+            End If
+            cont = cont + 1
+            GoTo suma
+        End If
+
+        TextBox4.Text = n(0)
+        TextBox5.Text = n(1)
+        TextBox6.Text = n(2)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        TextBox1.Text = 0
+        TextBox2.Text = 0
+        TextBox3.Text = 0
+
+    End Sub
+End Class
