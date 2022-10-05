5cb4d02671c77c460ed906b350f67d791fbd99c4
amldimsed <ads,dark@gmail.com>
2022-10-05 00:28:28 -0400

    calculadora-terminal python

---
+++ pregunta7/calculadora_python.py
@@ -0,0 +1,19 @@
+def suma(a,b):
+    return a+b
+def resta(a,b):
+    return a-b
+def multiplicacion(a,b):
+    return a*b
+def division(a,b):
+    return a/b
+
+num1=float(input("ingrese primer numero: "))
+num2=float(input("ingrese segundo numero: "))
+
+print(f'la suma es: {suma(num1,num2)}')
+print(f'la resta es: {resta(num1,num2)}')
+print(f'la multiplicacion es: {multiplicacion(num1,num2)}')
+print(f'la division es: {division(num1,num2)}')
+
+
+
